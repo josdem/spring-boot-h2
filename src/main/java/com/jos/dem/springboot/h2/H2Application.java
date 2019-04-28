6dc612f1dee1cd800f58f6e463c4f1485520e5eb
josdem <joseluis.delacruz@gmail.com>
2019-04-28 19:04:50 -0400

    Fixing identation

--- src/main/java/com/jos/dem/springboot/h2/H2Application.java
+++ src/main/java/com/jos/dem/springboot/h2/H2Application.java
@@ -1,10 +1,10 @@
 package com.example.springbooth2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.jos.dem.springboot.h2.model.Person;
@@ -13,7 +13,6 @@ import com.jos.dem.springboot.h2.repository.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 @SpringBootApplication
 @EntityScan("com.jos.dem.springboot.h2.model")
 @EnableJpaRepositories("com.jos.dem.springboot.h2.repository")
@@ -21,9 +20,9 @@ public class H2Application {
 
   private Logger log = LoggerFactory.getLogger(this.getClass());
 
-	public static void main(String[] args) {
-		SpringApplication.run(H2Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(H2Application.class, args);
+  }
 
   @Bean
   CommandLineRunner start(PersonRepository personRepository){
