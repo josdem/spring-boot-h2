b38accdc4181dda8b0ee03c69c7c457489a6ee4e
josdem <joseluis.delacruz@gmail.com>
2019-04-28 18:59:03 -0400

    Changing System.out.println by log info

--- src/main/java/com/jos/dem/springboot/h2/H2Application.java
+++ src/main/java/com/jos/dem/springboot/h2/H2Application.java
@@ -11,6 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import com.jos.dem.springboot.h2.model.Person;
 import com.jos.dem.springboot.h2.repository.PersonRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @SpringBootApplication
 @EntityScan("com.jos.dem.springboot.h2.model")
 @EnableJpaRepositories("com.jos.dem.springboot.h2.repository")
@@ -18,6 +22,8 @@ public class H2Application {
 
   @Autowired
   private PersonRepository personRepository;
+
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2Application.class, args);
@@ -28,7 +34,7 @@ public class H2Application {
     return args -> {
       Person person  = new Person(1L, "josdem", "joseluis.delacruz@gmail.com");
       personRepository.save(person);
-      personRepository.findAll().forEach(System.out::println);
+      personRepository.findAll().forEach(p -> log.info("person: {}", p));
     };
   }
 
