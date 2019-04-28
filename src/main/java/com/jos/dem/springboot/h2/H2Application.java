6faf947aad4e6862a9e70b07314e1ff310df0d0e
josdem <joseluis.delacruz@gmail.com>
2019-04-28 17:26:50 -0400

    Fixing compilation errors 2

--- src/main/java/com/jos/dem/springboot/h2/H2Application.java
+++ src/main/java/com/jos/dem/springboot/h2/H2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class H2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(H2Application.class, args);
 	}
 
 }
