f2d6f0ff3cd2dcc4c6e01dda8d0dd2b1f0bdec20
josdem <joseluis.delacruz@gmail.com>
2019-04-28 17:21:18 -0400

    Adding person entity

---
+++ src/main/java/com/jos/dem/springboot/h2/model/Person.java
@@ -0,0 +1,13 @@
+package com.jos.dem.springboot.h2;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nickname;
+	private String email;
